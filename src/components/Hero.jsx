5089f62c8f056edf1ffc8f93e21c502b5f78420e
Khushi2427 <khushigupta16057@gmail.com>
2026-03-05 17:12:34 +0530

    margin adjusted

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,11 +18,11 @@ const Hero = () => {
                 >
                     <h1>Home Interiors <br />Made <span className="accent-text italic">Easy</span></h1>
                     <p>Transform your living space with expert guidance from 400+ top designers. We handle everything from concept to flawless installation.</p>
-                    <div className="hero-btns">
+                    <div className="hero-btns" >
                         <Link to="/contact-us" className="btn hero-btn-primary">
                             Book Free Consultation <ArrowRight size={18} />
                         </Link>
-                        <Link to="/projects" className="btn hero-btn-outline">
+                        <Link to="/projects" className="btn hero-btn-outline" >
                             View Recent Projects
                         </Link>
                     </div>
